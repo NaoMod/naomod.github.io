58cb16e37b9377e5d47b93be212200f61b1298eb
Erwan Bousse <erwan.bousse@ls2n.fr>
2022-09-28 13:28:23 +0200

    Update dev scripts

--- run_with_docker.sh
+++ run_with_docker.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-sudo docker run -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve
+set -x
+set -e
+
+mkdir -p bundle
+
+docker rm -f jekyll
+
+docker run --rm \
+  -ti \
+  --volume="$PWD:/srv/jekyll:Z" \
+  --volume="$PWD/bundle:/usr/local/bundle:Z" \
+  --publish [::1]:4000:4000 \
+  --name jekyll \
+  jekyll/minimal \
+  bash serve.sh

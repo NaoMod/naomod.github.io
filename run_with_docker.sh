a3be1dc589b44f744144e2c33ff22b4606955383
ebousse <ebousse@users.noreply.github.com>
2022-09-28 06:27:10 -0500

    Deploying to gh-pages from @ NaoMod/naomod.github.io@955cad7c9d1a07aff47fdecc2939adf2483d9444 🚀

--- run_with_docker.sh
+++ run_with_docker.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-set -x
-set -e
-
-mkdir -p bundle
-
-docker rm -f jekyll
-
-docker run --rm \
-  -ti \
-  --volume="$PWD:/srv/jekyll:Z" \
-  --volume="$PWD/bundle:/usr/local/bundle:Z" \
-  --publish [::1]:4000:4000 \
-  --name jekyll \
-  jekyll/minimal \
-  bash serve.sh
+sudo docker run -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve
--- run_with_podman.sh
+++ run_with_podman.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# podman rm -f jekyll
+# podman run --rm \
+#     -p 4000:4000  \
+#     -v $(pwd):/site:Z \
+#     --name jekyll \
+#     bretfisher/jekyll-serve
+
 set -x
 set -e
 

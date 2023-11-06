f4b7d5d8ebcbc3e583c2210499613c3a795e0e4d
Erwan Bousse <erwan.bousse@ls2n.fr>
2023-11-06 15:09:05 +0100

    Make generation script use a list of speakers

--- seminar/generate_mondays.py
+++ seminar/generate_mondays.py
@@ -1,5 +1,7 @@
 import datetime
 from pathlib import Path
+import random
+from string import Template
 
 
 def on_day(date: datetime.date, day: int):
@@ -15,15 +17,30 @@ def on_day(date: datetime.date, day: int):
 
 
 def find_next_monday(date: datetime.date):
-    return onDay(date, 0)
+    return on_day(date, 0)
 
 
 ############
 # Input data
 ############
-start_date: datetime.date = datetime.date(2023, 11, 6)
-end_date: datetime.date = datetime.date(2024, 4, 1)
+start_date: datetime.date = datetime.date(2023, 11, 14)
 output_dir: Path = Path("./seminar/2324/_posts")
+speakers: list[str] = [
+    "Gerson Sunyé",
+    "Massimo Tisi",
+    "Erwan Bousse",
+    "Hugo Bruneliere",
+    "Théo Le Calvar",
+    "Jean-Marie Mottu",
+    "Dalila Tamzalit",
+    "Hiba Ajabri",
+    "Ali Benjilany",
+    "Matthew Coyle",
+    "Yasmina Daliyoucef",
+    "Josselin Enet",
+    "James Miranda",
+]
+random.shuffle(speakers)
 
 ############
 # Preparations
@@ -33,20 +50,19 @@ example_file: Path = Path("./seminar/example.md")
 if not example_file.exists():
     raise "Missing example file at location " + str(example_file)
 
+template: Template = Template(example_file.read_text())
+
 ############
 # Loop
 ############
-finished: bool = False
 current_date = start_date + datetime.timedelta(days=1)
 counter: int = 1
-while not finished:
+
+for speaker in speakers:
     next_monday: datetime.date = find_next_monday(current_date)
-    if next_monday > end_date:
-        finished = True
-    else:
-        file_name = str(next_monday) + "-talk" + str(counter) + ".md"
-        counter = counter + 1
-        output_file: Path = output_dir / file_name
-        output_file.write_text(example_file.read_text())
-        print("Created " + str(output_file))
-        current_date = next_monday + datetime.timedelta(days=1)
+    file_name = str(next_monday) + "-talk" + str(counter) + ".md"
+    counter = counter + 1
+    output_file: Path = output_dir / file_name
+    output_file.write_text(template.substitute(speaker=speaker))
+    print("Created " + str(output_file))
+    current_date = next_monday + datetime.timedelta(days=1)
